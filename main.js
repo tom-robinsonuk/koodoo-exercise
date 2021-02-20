7e6a0ff9c2b5387754a3f00947262f6a7f5d438d
Thomas Robinson <TDrobinsonUK@hotmail.com>
2021-02-20 17:42:16 +0000

    Updated Jest Test cases further checking for 0 and input changes -- including 5% difference

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const r = require('ramda')
 
-const calculateMonthlyPayment = (interestRate,loanValue) => {
+const calculateMonthlyPayememt = (interestRate,loanValue) => {
 	const monthlyInterestRate = ((interestRate/100) / 12)
 	const monthlyPayment = loanValue * monthlyInterestRate
 
@@ -11,7 +11,7 @@ const productSafetyCheck = (currentMonthlyPayment, expectedLoanValue, productArr
     const returnedProducts = []
     r.forEach( product => {
         if(product.repaymentType === 'interest-only') {
-            const expectedMonthlyPayment = calculateMonthlyPayment(product.interestRate, expectedLoanValue)
+            const expectedMonthlyPayment = calculateMonthlyPayememt(product.interestRate, expectedLoanValue)
             const tenPercentLimit = currentMonthlyPayment/10
             if((expectedMonthlyPayment - currentMonthlyPayment) < tenPercentLimit) {
                 returnedProducts.push(product)
@@ -20,11 +20,9 @@ const productSafetyCheck = (currentMonthlyPayment, expectedLoanValue, productArr
             returnedProducts.push(product)
         } 
     }, productArray)
-    console.log(returnedProducts);
     return returnedProducts;
 }
 
 module.exports = {
     productSafetyCheck
 }
-
--- main.test.js
+++ main.test.js
@@ -19,6 +19,26 @@ test('Ensure Repayment Mortgages are not filtered',() => {
     expect(productSafetyCheck(currentMonthlyPayment,loanValue,productList)).toEqual(expectedReturnedProducts)
 });
 
+/*Given:
+Repayment mortgage with current monthly payment of 0 (customer has no current mortgage)
+product should be returned*/
+test('Ensure Repayment mortgage with current monthly payment of 0, all products should be returned',() =>{
+    const productList = [{
+            productId:1,
+            repaymentType:"repayment",
+            interestRate:0.1
+       },{
+          productId:2,
+            repaymentType:"interest-only",
+            interestRate:1.2
+         }]
+
+    const expectedReturnedProducts = productList
+    const currentMonthlyPayment = 0
+    const loanValue = 100000
+
+    expect(productSafetyCheck(currentMonthlyPayment,loanValue,productList)).toEqual(expectedReturnedProducts)
+});
 
 /*Given: That the interest-only mortgage that is 10%  is also returned
 Test script fails: Criteria <10 which is 0 to 9 rather than 0 to 10
@@ -27,7 +47,7 @@ test('Interest-only product where monthly payment works out 10% difference',() =
     const productList = [{
         productId:1,
         repaymentType:"interest-only",
-        interestRate:"1.2" 
+        interestRate:1.2 
     }];
 
     const expectedReturnedProducts = productList
@@ -44,7 +64,7 @@ test('Interest-only product where monthly payment works out 9% difference',() =>
     const productList = [{
         productId:1,
         repaymentType:"interest-only",
-        interestRate:"1.2" 
+        interestRate:1.2 
     }];
 
     const expectedReturnedProducts = productList
@@ -54,18 +74,51 @@ test('Interest-only product where monthly payment works out 9% difference',() =>
     expect(productSafetyCheck(currentMonthlyPayment,loanValue,productList)).toEqual(expectedReturnedProducts)
 });
 
-/*Given: That interest-mortgage that is greater than 10% is not returned
+/*Given: That the interest-only mortgage that is 5%  is also returned
 */
-test('Interest-only product where monthly payment works out 15% difference',() =>{
+test('Interest-only product where monthly payment works out 5% difference',() =>{
     const productList = [{
         productId:1,
         repaymentType:"interest-only",
-        interestRate:"1.2" 
+        interestRate:1.217799
+    }];
+
+    const expectedReturnedProducts = productList
+    const currentMonthlyPayment = 210
+    const loanValue = 209000
+
+    expect(productSafetyCheck(currentMonthlyPayment,loanValue,productList)).toEqual(expectedReturnedProducts)
+});
+
+
+/*Given: That interest-mortgage that is greater than 10% is not returned
+*/
+test('Interest-only should not return product where monthly payment works out 15% difference',() =>{
+    const productList = [{
+        productId:1,
+        repaymentType:"interest-only",
+        interestRate:1.2
     }];
 
     const expectedReturnedProducts = productList
     const currentMonthlyPayment = 200
     const loanValue = 230000
 
-    expect(productSafetyCheck(currentMonthlyPayment,loanValue,productList)).toEqual(expectedReturnedProducts)
+    expect(productSafetyCheck(currentMonthlyPayment,loanValue,productList)).not.toEqual(expectedReturnedProducts)
+});
+
+/*Given: Interest-only does not return a product if strings are included
+*/
+test('Interest-only, letters should not return a product',() =>{
+    const productList = [{
+        productId:3,
+        repaymentType:"interest-only",
+        interestRate:16.5
+    }];
+
+    const expectedReturnedProducts = productList
+    const currentMonthlyPayment = "abcd"
+    const loanValue = "dgdsfgdsf"
+
+    expect(productSafetyCheck(currentMonthlyPayment,loanValue,productList)).not.toEqual(expectedReturnedProducts)
 });
